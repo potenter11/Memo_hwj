1304abbba3e4104d482be160e97fcd442d913ea3
DESKTOP-BD4B31F\user <tkdduq241@naver.com>
2022-03-15 16:39:45 +0900

    [황원준] - 새로운 메모 프로젝트!! - 최초 파일 추가

---
+++ src/main/java/com/potenter/memo/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.potenter.memo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "HelloWorld!!";
+	}
+	
+}
